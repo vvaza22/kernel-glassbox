41bc4945b467501ebc16e3fcd44cfccefdf0b875
vvaza22 <vvaza22@freeuni.edu.ge>
2026-05-09 14:32:02 +0400

    Add more nlclient tests

--- bridge/nlclient/nlclient_test.go
+++ bridge/nlclient/nlclient_test.go
@@ -8,12 +8,39 @@ import (
 )
 
 func TestNetlinkClient(t *testing.T) {
-	t.Run("connect and destroy", func(t *testing.T) {
+	t.Run("single connection", func(t *testing.T) {
 		nl, err := nlclient.NewNetlinkClient()
 		require.NoError(t, err)
 		require.NotNil(t, nl)
 
 		err = nl.Destroy()
 		require.NoError(t, err)
+	})
+
+	t.Run("multiple connections", func(t *testing.T) {
+		nl1, err := nlclient.NewNetlinkClient()
+		require.NoError(t, err)
+		require.NotNil(t, nl1)
+
+		nl2, err := nlclient.NewNetlinkClient()
+		require.NoError(t, err)
+		require.NotNil(t, nl2)
+
+		err = nl1.Destroy()
+		require.NoError(t, err)
+
+		err = nl2.Destroy()
+		require.NoError(t, err)
+	})
+
+	t.Run("rapid connect and destroy", func(t *testing.T) {
+		for range 100 {
+			nl, err := nlclient.NewNetlinkClient()
+			require.NoError(t, err)
+			require.NotNil(t, nl)
+
+			err = nl.Destroy()
+			require.NoError(t, err)
+		}
 	})
 }
