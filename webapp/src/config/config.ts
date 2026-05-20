adf51116fdc9faaee30d42ede6fe79ce5479e95f
vvaza22 <vvaza22@freeuni.edu.ge>
2026-05-20 20:34:52 +0400

    Update webapp port

--- webapp/src/config/config.ts
+++ webapp/src/config/config.ts
@@ -1,7 +1,7 @@
 import { LogLevel } from "@/types/logger";
 
 // WebSocket
-export const WS_URL = "ws://localhost:8080/ws";
+export const WS_URL = "ws://localhost:9090/ws";
 
 // Logger
 export const LOG_LEVEL = LogLevel.DEBUG;
