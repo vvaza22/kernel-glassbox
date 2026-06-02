43f126fbe37d01d329964c3f18fb53a8f230fa89
vvaza22 <vvaza22@freeuni.edu.ge>
2026-06-02 19:28:46 +0400

    Wrap toTreeNodes tests together

--- webapp/src/adapters/proctree.test.ts
+++ webapp/src/adapters/proctree.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { toTreeNodes } from "./proctree";
 import type { TreeNode } from "@/types/ui/proctree";
 import type { WebsocketTaskKey } from "@/types/ws/shared";
@@ -19,37 +19,219 @@ const rootTreeNode: TreeNode = {
   childTreeNodeIds: [],
 };
 
-test("toTreeNodes returns root node on empty input", () => {
-  expect(toTreeNodes([])).toEqual([rootTreeNode]);
-});
+describe("toTreeNodes", () => {
+  test("returns root node on empty input", () => {
+    expect(toTreeNodes([])).toEqual([rootTreeNode]);
+  });
 
-test("toTreeNodes handles root with multiple children", () => {
-  const keyA: WebsocketTaskKey = { pid: "1", startTime: "100" };
-  const keyB: WebsocketTaskKey = { pid: "2", startTime: "200" };
+  test("handles root with multiple children", () => {
+    const keyA: WebsocketTaskKey = { pid: "1", startTime: "100" };
+    const keyB: WebsocketTaskKey = { pid: "2", startTime: "200" };
 
-  const nodes = [
-    {
-      parent: fakeKey,
-      realParent: rootKey,
-      groupLeader: keyA,
-      self: keyA,
-      name: "A",
-    },
-    {
-      parent: fakeKey,
-      realParent: rootKey,
-      groupLeader: keyB,
-      self: keyB,
-      name: "B",
-    },
-  ];
+    const nodes = [
+      {
+        parent: fakeKey,
+        realParent: rootKey,
+        groupLeader: keyA,
+        self: keyA,
+        name: "A",
+      },
+      {
+        parent: fakeKey,
+        realParent: rootKey,
+        groupLeader: keyB,
+        self: keyB,
+        name: "B",
+      },
+    ];
 
-  const expectedNodes: TreeNode[] = [
-    {
+    const expectedNodes: TreeNode[] = [
+      {
+        ...rootTreeNode,
+        childTreeNodeIds: ["1-100", "2-200"],
+      },
+      {
+        id: "1-100",
+        parentId: "0-0",
+        parentTreeNodeId: "0-0",
+        name: "A",
+        pid: keyA.pid,
+        startTime: keyA.startTime,
+        subNodes: [],
+        childTreeNodeIds: [],
+      },
+      {
+        id: "2-200",
+        parentId: "0-0",
+        parentTreeNodeId: "0-0",
+        name: "B",
+        pid: keyB.pid,
+        startTime: keyB.startTime,
+        subNodes: [],
+        childTreeNodeIds: [],
+      },
+    ];
+
+    expect(toTreeNodes(nodes)).toEqual(expectedNodes);
+  });
+
+  test("handles sub nodes", () => {
+    const keyA: WebsocketTaskKey = { pid: "10", startTime: "100" };
+    const keyB: WebsocketTaskKey = { pid: "20", startTime: "200" };
+    const nodes: ProctreeNode[] = [
+      {
+        parent: fakeKey,
+        realParent: rootKey,
+        groupLeader: keyA,
+        self: keyA,
+        name: "A",
+      },
+      {
+        parent: fakeKey,
+        realParent: rootKey,
+        groupLeader: keyA,
+        self: { pid: "11", startTime: "110" },
+        name: "A1",
+      },
+      {
+        parent: fakeKey,
+        realParent: rootKey,
+        groupLeader: keyB,
+        self: keyB,
+        name: "B",
+      },
+      {
+        parent: fakeKey,
+        realParent: rootKey,
+        groupLeader: keyB,
+        self: { pid: "21", startTime: "210" },
+        name: "B1",
+      },
+      {
+        parent: fakeKey,
+        realParent: rootKey,
+        groupLeader: keyB,
+        self: { pid: "22", startTime: "220" },
+        name: "B2",
+      },
+    ];
+    const expectedNodes: TreeNode[] = [
+      {
+        ...rootTreeNode,
+        childTreeNodeIds: ["10-100", "20-200"],
+      },
+      {
+        id: "10-100",
+        parentId: "0-0",
+        parentTreeNodeId: "0-0",
+        name: "A",
+        pid: keyA.pid,
+        startTime: keyA.startTime,
+        subNodes: [
+          {
+            id: "11-110",
+            name: "A1",
+            pid: "11",
+          },
+        ],
+        childTreeNodeIds: [],
+      },
+      {
+        id: "20-200",
+        parentId: "0-0",
+        parentTreeNodeId: "0-0",
+        name: "B",
+        pid: keyB.pid,
+        startTime: keyB.startTime,
+        subNodes: [
+          {
+            id: "21-210",
+            name: "B1",
+            pid: "21",
+          },
+          {
+            id: "22-220",
+            name: "B2",
+            pid: "22",
+          },
+        ],
+        childTreeNodeIds: [],
+      },
+    ];
+    expect(toTreeNodes(nodes)).toEqual(expectedNodes);
+  });
+
+  test("handles deep trees", () => {
+    const keyA = { pid: "1", startTime: "100" };
+    const keyB = { pid: "2", startTime: "200" };
+    const keyC = { pid: "3", startTime: "300" };
+    const keyD = { pid: "40", startTime: "4000" };
+    const keyD1 = { pid: "41", startTime: "4100" };
+    const keyE = { pid: "5", startTime: "500" };
+    const keyF = { pid: "6", startTime: "600" };
+
+    const nodes: ProctreeNode[] = [
+      {
+        parent: fakeKey,
+        realParent: rootKey,
+        groupLeader: keyA,
+        self: keyA,
+        name: "A",
+      },
+      {
+        parent: fakeKey,
+        realParent: keyA,
+        groupLeader: keyB,
+        self: keyB,
+        name: "B",
+      },
+      {
+        parent: fakeKey,
+        realParent: rootKey,
+        groupLeader: keyC,
+        self: keyC,
+        name: "C",
+      },
+      {
+        parent: fakeKey,
+        realParent: keyC,
+        groupLeader: keyD,
+        self: keyD,
+        name: "D",
+      },
+      {
+        parent: fakeKey,
+        realParent: fakeKey,
+        groupLeader: keyD,
+        self: keyD1,
+        name: "D1",
+      },
+      {
+        parent: fakeKey,
+        realParent: keyD1,
+        groupLeader: keyE,
+        self: keyE,
+        name: "E",
+      },
+      {
+        parent: fakeKey,
+        realParent: keyD,
+        groupLeader: keyF,
+        self: keyF,
+        name: "F",
+      },
+    ];
+
+    const result = toTreeNodes(nodes);
+    expect(result).toHaveLength(7); // 6 main nodes + root
+    expect(result[0]).toEqual({
       ...rootTreeNode,
-      childTreeNodeIds: ["1-100", "2-200"],
-    },
-    {
+      childTreeNodeIds: ["1-100", "3-300"],
+    });
+
+    // The order of other nodes is not guaranteed
+
+    expect(result).toContainEqual({
       id: "1-100",
       parentId: "0-0",
       parentTreeNodeId: "0-0",
@@ -57,248 +239,68 @@ test("toTreeNodes handles root with multiple children", () => {
       pid: keyA.pid,
       startTime: keyA.startTime,
       subNodes: [],
-      childTreeNodeIds: [],
-    },
-    {
+      childTreeNodeIds: ["2-200"],
+    });
+
+    expect(result).toContainEqual({
       id: "2-200",
-      parentId: "0-0",
-      parentTreeNodeId: "0-0",
+      parentId: "1-100",
+      parentTreeNodeId: "1-100",
       name: "B",
       pid: keyB.pid,
       startTime: keyB.startTime,
       subNodes: [],
       childTreeNodeIds: [],
-    },
-  ];
+    });
 
-  expect(toTreeNodes(nodes)).toEqual(expectedNodes);
-});
-
-test("toTreeNodes handles sub nodes", () => {
-  const keyA: WebsocketTaskKey = { pid: "10", startTime: "100" };
-  const keyB: WebsocketTaskKey = { pid: "20", startTime: "200" };
-  const nodes: ProctreeNode[] = [
-    {
-      parent: fakeKey,
-      realParent: rootKey,
-      groupLeader: keyA,
-      self: keyA,
-      name: "A",
-    },
-    {
-      parent: fakeKey,
-      realParent: rootKey,
-      groupLeader: keyA,
-      self: { pid: "11", startTime: "110" },
-      name: "A1",
-    },
-    {
-      parent: fakeKey,
-      realParent: rootKey,
-      groupLeader: keyB,
-      self: keyB,
-      name: "B",
-    },
-    {
-      parent: fakeKey,
-      realParent: rootKey,
-      groupLeader: keyB,
-      self: { pid: "21", startTime: "210" },
-      name: "B1",
-    },
-    {
-      parent: fakeKey,
-      realParent: rootKey,
-      groupLeader: keyB,
-      self: { pid: "22", startTime: "220" },
-      name: "B2",
-    },
-  ];
-  const expectedNodes: TreeNode[] = [
-    {
-      ...rootTreeNode,
-      childTreeNodeIds: ["10-100", "20-200"],
-    },
-    {
-      id: "10-100",
+    expect(result).toContainEqual({
+      id: "3-300",
       parentId: "0-0",
       parentTreeNodeId: "0-0",
-      name: "A",
-      pid: keyA.pid,
-      startTime: keyA.startTime,
-      subNodes: [
-        {
-          id: "11-110",
-          name: "A1",
-          pid: "11",
-        },
-      ],
-      childTreeNodeIds: [],
-    },
-    {
-      id: "20-200",
-      parentId: "0-0",
-      parentTreeNodeId: "0-0",
-      name: "B",
-      pid: keyB.pid,
-      startTime: keyB.startTime,
-      subNodes: [
-        {
-          id: "21-210",
-          name: "B1",
-          pid: "21",
-        },
-        {
-          id: "22-220",
-          name: "B2",
-          pid: "22",
-        },
-      ],
-      childTreeNodeIds: [],
-    },
-  ];
-  expect(toTreeNodes(nodes)).toEqual(expectedNodes);
-});
-
-test("toTreeNodes handles deep trees", () => {
-  const keyA = { pid: "1", startTime: "100" };
-  const keyB = { pid: "2", startTime: "200" };
-  const keyC = { pid: "3", startTime: "300" };
-  const keyD = { pid: "40", startTime: "4000" };
-  const keyD1 = { pid: "41", startTime: "4100" };
-  const keyE = { pid: "5", startTime: "500" };
-  const keyF = { pid: "6", startTime: "600" };
-
-  const nodes: ProctreeNode[] = [
-    {
-      parent: fakeKey,
-      realParent: rootKey,
-      groupLeader: keyA,
-      self: keyA,
-      name: "A",
-    },
-    {
-      parent: fakeKey,
-      realParent: keyA,
-      groupLeader: keyB,
-      self: keyB,
-      name: "B",
-    },
-    {
-      parent: fakeKey,
-      realParent: rootKey,
-      groupLeader: keyC,
-      self: keyC,
       name: "C",
-    },
-    {
-      parent: fakeKey,
-      realParent: keyC,
-      groupLeader: keyD,
-      self: keyD,
+      pid: keyC.pid,
+      startTime: keyC.startTime,
+      subNodes: [],
+      childTreeNodeIds: ["40-4000"],
+    });
+
+    expect(result).toContainEqual({
+      id: "40-4000",
+      parentId: "3-300",
+      parentTreeNodeId: "3-300",
       name: "D",
-    },
-    {
-      parent: fakeKey,
-      realParent: fakeKey,
-      groupLeader: keyD,
-      self: keyD1,
-      name: "D1",
-    },
-    {
-      parent: fakeKey,
-      realParent: keyD1,
-      groupLeader: keyE,
-      self: keyE,
+      pid: keyD.pid,
+      startTime: keyD.startTime,
+      subNodes: [
+        {
+          id: "41-4100",
+          name: "D1",
+          pid: keyD1.pid,
+        },
+      ],
+      childTreeNodeIds: ["5-500", "6-600"],
+    });
+
+    expect(result).toContainEqual({
+      id: "5-500",
+      parentId: "41-4100",
+      parentTreeNodeId: "40-4000",
       name: "E",
-    },
-    {
-      parent: fakeKey,
-      realParent: keyD,
-      groupLeader: keyF,
-      self: keyF,
+      pid: keyE.pid,
+      startTime: keyE.startTime,
+      subNodes: [],
+      childTreeNodeIds: [],
+    });
+
+    expect(result).toContainEqual({
+      id: "6-600",
+      parentId: "40-4000",
+      parentTreeNodeId: "40-4000",
       name: "F",
-    },
-  ];
-
-  const result = toTreeNodes(nodes);
-  expect(result).toHaveLength(7); // 6 main nodes + root
-  expect(result[0]).toEqual({
-    ...rootTreeNode,
-    childTreeNodeIds: ["1-100", "3-300"],
-  });
-
-  // The order of other nodes is not guaranteed
-
-  expect(result).toContainEqual({
-    id: "1-100",
-    parentId: "0-0",
-    parentTreeNodeId: "0-0",
-    name: "A",
-    pid: keyA.pid,
-    startTime: keyA.startTime,
-    subNodes: [],
-    childTreeNodeIds: ["2-200"],
-  });
-
-  expect(result).toContainEqual({
-    id: "2-200",
-    parentId: "1-100",
-    parentTreeNodeId: "1-100",
-    name: "B",
-    pid: keyB.pid,
-    startTime: keyB.startTime,
-    subNodes: [],
-    childTreeNodeIds: [],
-  });
-
-  expect(result).toContainEqual({
-    id: "3-300",
-    parentId: "0-0",
-    parentTreeNodeId: "0-0",
-    name: "C",
-    pid: keyC.pid,
-    startTime: keyC.startTime,
-    subNodes: [],
-    childTreeNodeIds: ["40-4000"],
-  });
-
-  expect(result).toContainEqual({
-    id: "40-4000",
-    parentId: "3-300",
-    parentTreeNodeId: "3-300",
-    name: "D",
-    pid: keyD.pid,
-    startTime: keyD.startTime,
-    subNodes: [
-      {
-        id: "41-4100",
-        name: "D1",
-        pid: keyD1.pid,
-      },
-    ],
-    childTreeNodeIds: ["5-500", "6-600"],
-  });
-
-  expect(result).toContainEqual({
-    id: "5-500",
-    parentId: "41-4100",
-    parentTreeNodeId: "40-4000",
-    name: "E",
-    pid: keyE.pid,
-    startTime: keyE.startTime,
-    subNodes: [],
-    childTreeNodeIds: [],
-  });
-
-  expect(result).toContainEqual({
-    id: "6-600",
-    parentId: "40-4000",
-    parentTreeNodeId: "40-4000",
-    name: "F",
-    pid: keyF.pid,
-    startTime: keyF.startTime,
-    subNodes: [],
-    childTreeNodeIds: [],
+      pid: keyF.pid,
+      startTime: keyF.startTime,
+      subNodes: [],
+      childTreeNodeIds: [],
+    });
   });
 });
